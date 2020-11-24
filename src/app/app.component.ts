2a8dc35a533a494ad45eff27ebc5a56b2deefb9b
Tesalon Felicien <felicien.tesalon@gmail.com>
2020-11-24 13:55:58 -0500

    Adding new Angular Quiz to Repository

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+// src/app/app.component.ts
+import { Component } from '@angular/core';
+import { QuestionsService } from './questions.service';
+import { Quiz, Answers, Choice } from './quiz.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  answers: Answers;
+  quiz: Quiz;
+  currentQuestionIndex: number;
+  private showResults = false;
+
+  constructor(private questionsService: QuestionsService ) {
+    this.questionsService.getJSON('french').subscribe(data => {
+
+      this.quiz = new Quiz('french', data);
+      this.answers = new Answers();
+      this.currentQuestionIndex = 0;
+    });
+  }
+
+  updateChoice(choice: Choice) {
+    this.answers.values[this.currentQuestionIndex] = choice;
+  }
+
+  nextOrViewResults() {
+    if (this.currentQuestionIndex === this.quiz.questions.length - 1) {
+      this.showResults = true;
+      return;
+    }
+
+    this.currentQuestionIndex++;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
